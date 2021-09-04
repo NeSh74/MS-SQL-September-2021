a715000d7114305ebee739c2921818f218f6cc7b
NeSh74 <nejdie@yahoo.com>
2021-09-04 22:45:17 +0300

    new

---
+++ 3. CRUD/10. Find Names of All Employees.sql
@@ -0,0 +1,6 @@
+--Write a SQL query to find the full name of all employees whose salary is 25000, 14000, 12500 or 23600. Full Name is combination of first, middle and last name (separated with single space) and they should be in one column called “Full Name”.
+
+SELECT 
+	CONCAT(FirstName, ' ', MiddleName, ' ', LastName) AS [Full Name] 
+	FROM Employees
+	WHERE Salary IN (25000, 14000, 12500, 23600)	
