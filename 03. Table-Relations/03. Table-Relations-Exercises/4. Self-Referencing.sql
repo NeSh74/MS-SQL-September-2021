f9ef3b66932438a1935b7d1677504bf28dd44729
NeSh74 <nejdie@yahoo.com>
2021-09-23 16:31:59 +0300

    Update

--- 03. Table-Relations/03. Table-Relations-Exercises/4. Self-Referencing.sql
+++ 03. Table-Relations/03. Table-Relations-Exercises/4. Self-Referencing.sql
@@ -1,15 +1,20 @@
-CREATE TABLE Teachers
-(
-	TeacherID INT PRIMARY KEY,
-	[Name] NVARCHAR(50) NOT NULL,
-	ManagerID INT FOREIGN KEY REFERENCES Teachers(TeacherID)
+--CREATE DATABASE [EntityRelationsDemo2021]
+
+--USE [EntityRelationsDemo2021]
+
+CREATE TABLE [Teachers](
+						[TeacherID] INT PRIMARY KEY IDENTITY(101, 1) NOT NULL,
+						[Name] VARCHAR(50) NOT NULL,
+						[ManagerID] INT FOREIGN KEY REFERENCES [Teachers]([TeacherID]) --CAN BE NULL, SEE THE CELL 1
 )
 
-INSERT INTO Teachers(TeacherID, [Name], ManagerID)
-Values
-(101, 'John', NULL),
-(102, 'Maya', 106),
-(103, 'Silvia', 106),
-(104, 'Ted', 105),
-(105, 'Mark', 101),
-(106, 'Greta', 101)
+INSERT INTO [Teachers]([Name], [ManagerID])
+	VALUES
+			('John', NULL),
+			('Maya', 106),
+			('Silvia', 106),
+			('Ted', 105),
+			('Mark', 101),
+			('Greta', 101)
+
+--SELECT * FROM [Teachers]
