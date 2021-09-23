a0d6bc64339c9e32b1da48a550dd6240f3e5b997
NeSh74 <nejdie@yahoo.com>
2021-09-23 12:11:53 +0300

    Update

--- 03. Table-Relations/03. Table-Relations-Exercises/3. Many-To-Many Relationship.sql
+++ 03. Table-Relations/03. Table-Relations-Exercises/3. Many-To-Many Relationship.sql
@@ -1,42 +1,49 @@
-CREATE TABLE Students
-(
-	StudentID INT PRIMARY KEY,
-	[Name] NVARCHAR(50) NOT NULL
+--CREATE DATABASE [EntityRelationsDemo2021]
+
+--USE [EntityRelationsDemo2021]
+
+CREATE TABLE [Students](
+						[StudentID] INT PRIMARY KEY IDENTITY NOT NULL,
+						[Name] VARCHAR(50) NOT NULL,
 )
 
-CREATE TABLE Exams
-(
-	ExamID INT PRIMARY KEY,
-	[Name] NVARCHAR(80) NOT NULL
+CREATE TABLE [Exams](
+					[ExamID] INT PRIMARY KEY IDENTITY(101, 1) NOT NULL,
+					[Name] NVARCHAR(75) NOT NULL
 )
 
-CREATE TABLE StudentsExams
-(
-	StudentID INT NOT NULL FOREIGN KEY REFERENCES Students(StudentID),
-	ExamID INT NOT NULL FOREIGN KEY REFERENCES Exams(ExamID),
-	PRIMARY KEY(StudentID, ExamID) --Composit Primary Key
+CREATE TABLE [StudentsExams](
+					[StudentID] INT FOREIGN KEY REFERENCES [Students]([StudentID]) NOT NULL,
+					[ExamID] INT FOREIGN KEY REFERENCES [Exams]([ExamID]) NOT NULL,
+					PRIMARY KEY ([StudentID], [ExamID]) --Composit Primary Key
 )
 
-INSERT INTO Students(StudentID, [Name])
-VALUES
-(1, 'Mila'),
-(2, 'Toni'),
-(3, 'Ron')
+INSERT INTO [Students]([Name]) 
+	VALUES
+			('Mila'),                                      
+			('Toni'),
+			('Ron')
 
-INSERT INTO Exams(ExamID, [Name])
-VALUES
-(101, 'SpringMVC'),
-(102, 'Neo4j'),
-(103, 'Oracle 11g')
+INSERT INTO [Exams]([Name]) 
+	VALUES
+			('SpringMVC'),
+			('Neo4j'),
+			('Oracle 11g')
 
-INSERT INTO StudentsExams(StudentID,ExamID)
-VALUES
-(1, 101),
-(1, 102),
-(2, 101),
-(3, 103),
-(2, 102),
-(2, 103)
+INSERT INTO [StudentsExams]([StudentID], [ExamID]) 
+	VALUES
+			(1, 101),
+			(1,	102),
+			(2,	101),
+			(3,	103),
+			(2,	102),
+			(2,	103)
+
+
+
+--SELECT * FROM [Students]
+--SELECT * FROM [Exams]
+--SELECT * FROM [StudentsExams]
 
 --SELECT s.[Name], e.[Name] FROM StudentsExams AS se
 --JOIN Students AS s ON se.StudentID = s.StudentID 
