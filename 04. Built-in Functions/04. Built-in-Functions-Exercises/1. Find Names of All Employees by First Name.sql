4ce9f1dfe9dff02f57e9cec13c0242ce116556a3
NeSh74 <nejdie@yahoo.com>
2021-09-26 21:04:41 +0300

    update

--- 04. Built-in Functions/04. Built-in-Functions-Exercises/1. Find Names of All Employees by First Name.sql
+++ 04. Built-in Functions/04. Built-in-Functions-Exercises/1. Find Names of All Employees by First Name.sql
@@ -1,4 +1,11 @@
 --USE [SoftUni]
 
-SELECT Firstname, LastName FROM Employees
-WHERE FirstName LIKE 'SA%'
+--SELECT [FirstName], [LastName] FROM [Employees]
+--WHERE LEFT([FirstName], 2) = 'Sa'
+
+--SELECT [FirstName], [LastName] FROM [Employees]
+--WHERE SUBSTRING([FirstName], 1, 2) = 'Sa'
+
+SELECT [Firstname], [LastName] FROM [Employees]
+--WHERE [FirstName] LIKE 'SA%'
+WHERE [FirstName] LIKE 'SA%'
--- 04. Built-in Functions/04. Built-in-Functions-Exercises/2. Find Names of All employees by Last Name.sql
+++ 04. Built-in Functions/04. Built-in-Functions-Exercises/2. Find Names of All employees by Last Name.sql
@@ -1,4 +1,13 @@
 --USE [SoftUni]
 
-SELECT Firstname, LastName FROM Employees
-WHERE LastName LIKE '%ei%'
+--SELECT [FirstName], [LastName], CHARINDEX('ei', [LastName]) FROM [Employees]
+--I VARIANT
+
+--SELECT [FirstName], [LastName] FROM [Employees]
+--WHERE CHARINDEX('ei', [LastName]) <> 0
+
+--II VARIANT
+
+SELECT [FirstName], [LastName] FROM [Employees]
+WHERE [LastName] LIKE '%ei%'
+
