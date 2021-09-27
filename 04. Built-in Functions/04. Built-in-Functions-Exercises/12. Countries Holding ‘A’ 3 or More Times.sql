e8d6ca0f258d0ce2993c15f9dfce6ea31c6ff2a9
NeSh74 <nejdie@yahoo.com>
2021-09-27 11:24:23 +0300

    new

--- 04. Built-in Functions/04. Built-in-Functions-Exercises/12. Countries Holding ‘A’ 3 or More Times.sql
+++ 04. Built-in Functions/04. Built-in-Functions-Exercises/12. Countries Holding ‘A’ 3 or More Times.sql
@@ -1,6 +1,8 @@
 --USE [Geography]
 
-SELECT CountryName AS [Country Name], IsoCode AS [Iso Code] 
-FROM Countries
-WHERE CountryName LIKE '%a%a%a%'
-ORDER BY IsoCode
+  SELECT [CountryName] AS [Country Name], 
+	     [IsoCode]	   AS [ISO Code] 
+    FROM [Countries]
+   WHERE [CountryName] LIKE '%A%A%A%'
+ORDER BY [IsoCode]
+
